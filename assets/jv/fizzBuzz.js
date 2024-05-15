0e06c1f1a774936aedf1f8b488128bfb267b5912
esquivel mariano <esquivelmariano@18gmail.com>
2024-05-15 14:37:51 -0300

    finalice los trabajos

--- assets/jv/fizzBuzz.js
+++ assets/jv/fizzBuzz.js
@@ -1,9 +1,3 @@
-// 3) FizzBuzz. Generar un programa que itere del 1 al 15. Por cada iteración:
-// a) si el numero es múltiplo de 3, mostrar la palabra "Fizz"
-// b) si el numero es múltiplo de 5, mostrar la palabra "Buzz"
-// c) si el numero es multiplo de 3 y 5, mostrar la palabra "FizzBuzz"
-// d) en los demas casos mostrar solo el número
-
 for (let i = 1; i <= 15; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
         alert("FizzBuzz");
--- assets/jv/triangulo.js
+++ assets/jv/triangulo.js
@@ -1,10 +1,3 @@
-// 2) Triangulo de #. Para este ejercicio, construir un programa que permita armar un trianguloformado con #, de la siguiente manera:
-// #
-// ##
-// ###
-// ####
-// #####
-
 for (let i = 1; i <= 5; i++) {
     let triangulo = "";
     for (let j = 1; j <= i; j++) {
